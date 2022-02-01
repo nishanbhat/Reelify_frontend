c2c0cb5d39605ee59124c784cf06f874b8450ba7
nishanbhat <rananishanbhat@gmail.com>
2022-02-01 13:06:41 +0545

    made template for video  detail page

--- src/components/views/DetailVideoPage/DetailVideoPage.js
+++ src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { List, Avatar, Typography } from 'antd';
 
 function DetailVideoPage() {
-  return <div>Detailed Page</div>;
+  return (
+  <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
+  <video style={{ width: '100%' }} src controls></video>
+
+  <List.Item
+      actions={[]}
+  >
+      <List.Item.Meta
+          avatar={<Avatar src />}
+          title={<a href="https://ant.design">user name</a>}
+          description
+      />
+      <div></div>
+  </List.Item>
+
+</div>
+  )
+
 }
 
 export default DetailVideoPage;
+
