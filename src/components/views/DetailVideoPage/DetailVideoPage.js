fabf3d23ef96a1346af0a9c147b483b5c8ca9264
nishanbhat <rananishanbhat@gmail.com>
2022-02-02 21:25:22 +0545

    video detail page added

--- src/components/views/DetailVideoPage/DetailVideoPage.js
+++ src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { List, Avatar, Typography } from 'antd';
+import axios from 'axios';
 
-function DetailVideoPage() {
+function DetailVideoPage(props) {
+    const videoId = props.match.params.videoId
+    const [Video, setVideo] = useState([])
+
+    const videoVariable = {
+        videoId: videoId
+    }
+
+            useEffect(() => {
+                axios.post('/api/video/getVideo', videoVariable).then(response => {
+                    if (response.data.success) { 
+                        setVideo(response.data.video)
+                    }
+                    else{alert('fail')}
+                })
+            }, []);
+
   return (
   <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
-  <video style={{ width: '100%' }} src controls></video>
+  <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
 
   <List.Item
       actions={[]}
   >
       <List.Item.Meta
-          avatar={<Avatar src />}
-          title={<a href="https://ant.design">user name</a>}
-          description
+          avatar={<Avatar src={Video.writer && Video.writer.image} />}
+          title={<a href="https://ant.design">{Video.title}</a>}
+          description={Video.description}
       />
       <div></div>
   </List.Item>
 
 </div>
-  )
+)
+  }
 
-}
 
 export default DetailVideoPage;
-
