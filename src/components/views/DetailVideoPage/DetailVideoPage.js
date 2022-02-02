0f758f2c719fac92188579546d5e0107147b2a61
nishanbhat <rananishanbhat@gmail.com>
2022-02-02 21:47:15 +0545

    layout for side video added

--- src/components/views/DetailVideoPage/DetailVideoPage.js
+++ src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { List, Avatar, Typography } from 'antd';
+import { List, Avatar, Row, Col } from 'antd';
 import axios from 'axios';
 
 function DetailVideoPage(props) {
@@ -20,21 +20,29 @@ function DetailVideoPage(props) {
             }, []);
 
   return (
-  <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
-  <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
+    <Row>
+    <Col lg={18} xs={24}>
+        <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
+            <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
 
-  <List.Item
-      actions={[]}
-  >
-      <List.Item.Meta
-          avatar={<Avatar src={Video.writer && Video.writer.image} />}
-          title={<a href="https://ant.design">{Video.title}</a>}
-          description={Video.description}
-      />
-      <div></div>
-  </List.Item>
+            <List.Item
+                actions={[]}
+            >
+                <List.Item.Meta
+                    avatar={<Avatar src={Video.writer && Video.writer.image} />}
+                    title={<a href="https://ant.design">{Video.title}</a>}
+                    description={Video.description}
+                />
+                <div></div>
+            </List.Item>
 
-</div>
+        </div>
+    </Col>
+    <Col lg={6} xs={24}>
+        For Side Video
+    </Col>
+</Row>
+  
 )
   }
 
