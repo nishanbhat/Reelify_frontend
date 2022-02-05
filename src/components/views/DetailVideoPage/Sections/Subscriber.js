6041844876efdb246594a21c25ad995ec9778c07
nishanbhat <rananishanbhat@gmail.com>
2022-02-05 14:59:00 +0545

    color changed for subscribe buttom

--- src/components/views/DetailVideoPage/Sections/Subscriber.js
+++ src/components/views/DetailVideoPage/Sections/Subscriber.js
@@ -4,7 +4,7 @@ function Subscriber(props) {
     return (
         <div>
             <button style={{
-                backgroundColor: '#CC', borderRadius: '4px', color: 'white',
+                backgroundColor: 'black', borderRadius: '4px', color: 'white',
                 padding: '10px 16px', fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase'
             }}>
                     Subscribe
