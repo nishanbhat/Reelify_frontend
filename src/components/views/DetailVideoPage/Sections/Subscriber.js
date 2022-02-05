a34defacf81b0d593be118c48eb88fbe7df9c914
nishanbhat <rananishanbhat@gmail.com>
2022-02-05 15:38:58 +0545

    populating the subscribing data on browser

--- src/components/views/DetailVideoPage/Sections/Subscriber.js
+++ src/components/views/DetailVideoPage/Sections/Subscriber.js
@@ -1,13 +1,46 @@
 import React, { useEffect, useState } from 'react'
-function Subscriber(props) {
+import axios from 'axios';
 
+function Subscriber(props) {
+    
+    const userTo = props.userTo
+    const userFrom = props.userFrom
+
+    const [subscribeNumber, setSubscriberNumber] = useState(0)
+    const [Subscribed, setSubscribed] = useState(false)
+
+
+    useEffect(() => {
+
+        const subscribeNumberVariables = { userTo: userTo, userFrom: userFrom }
+        axios.post('/api/subscribe/subscribeNumber', subscribeNumberVariables)
+            .then(response => {
+                if (response.data.success) {
+                    setSubscriberNumber(response.data.subscribeNumber)
+                } else {
+                    alert('Failed to get subscriber Number')
+                }
+            })
+
+            axios.post('/api/subscribe/subscribed', subscribeNumberVariables)
+            .then(response => {
+                if (response.data.success) {
+                    setSubscribed(response.data.subcribed)
+                } else {
+                    alert('Failed to get Subscribed Information')
+                }
+            })
+
+        },[])
     return (
         <div>
             <button style={{
-                backgroundColor: 'black', borderRadius: '4px', color: 'white',
+                backgroundColor: `${Subscribed ? '#AAAAA' : 'Black'}`,
+                borderRadius: '4px', color: 'white',
                 padding: '10px 16px', fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase'
             }}>
-                    Subscribe
+                {subscribeNumber} {Subscribed ? 'Subscribed' :
+                    'Subscribe'}
             </button>
         </div>
     )
