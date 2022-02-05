fd29bd244db91a5bb15de3dc2ba3defdb0b9d4bb
nishanbhat <rananishanbhat@gmail.com>
2022-02-05 14:41:14 +0545

    added side videos in video detail page

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -21,18 +21,14 @@ function LandingPage() {
             })
     }, [])
 
-
-
-
-
     const renderCards = Videos.map((video, index) => {
-
+        console.log(video)
         var minutes = Math.floor(video.duration / 60);
         var seconds = Math.floor(video.duration - minutes * 60);
 
         return <Col lg={6} md={8} xs={24}>
             <div style={{ position: 'relative' }}>
-                <a href={'/video/video.id'}>
+                <a href={`/video/${video._id}`}>
                 <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
                 <div className=" duration"
                     style={{ bottom: 0, right:0, position: 'absolute', margin: '4px', 
@@ -55,8 +51,6 @@ function LandingPage() {
         </Col>
 
     })
-
-
 
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
