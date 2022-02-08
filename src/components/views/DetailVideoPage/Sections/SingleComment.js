3e511b5a09ca03fc04ef77ac82df042a5a1c3fea
nishanbhat <rananishanbhat@gmail.com>
2022-02-08 12:09:13 +0545

    made template for single comment

---
+++ src/components/views/DetailVideoPage/Sections/SingleComment.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { Comment, Avatar, Button, Input } from 'antd';
+
+function SingleComment(props) {
+    
+    const actions = [
+        <span onClick key="comment-basic-reply-to">Reply to </span>
+    ]
+
+    return (
+        <div>
+            <Comment
+                actions={actions}
+                author={props.comment.writer.name}
+                avatar={
+                    <Avatar
+                        src={props.comment.writer.image}
+                        alt="image"
+                    />
+                }
+                content={
+                    <p>
+                        {props.comment.content}
+                    </p>
+                }
+            ></Comment>
+
+                <form style={{ display: 'flex' }} onSubmit={onSubmit}>
+                    <TextArea
+                        style={{ width: '100%', borderRadius: '5px' }}
+                        onChange={handleChange}
+                        value={CommentValue}
+                        placeholder="write some comments"
+                    />
+                    <br />
+                    <Button style={{ width: '20%', height: '52px' }} onClick={onSubmit}>Submit</Button>
+                </form>
+
+        </div>
+    )
+}
+
+export default SingleComment
