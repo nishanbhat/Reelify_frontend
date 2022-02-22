6a80c0d61102919a0d6f806f38b299d2e37dcfdb
nishanbhat <rananishanbhat@gmail.com>
2022-02-22 22:57:08 +0545

    imported likes and dislikes

--- src/components/views/DetailVideoPage/Sections/SingleComment.js
+++ src/components/views/DetailVideoPage/Sections/SingleComment.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Comment, Avatar, Button, Input } from 'antd';
 import Axios from 'axios';
 import { useSelector } from 'react-redux';
+import LikeDislikes from './LikeDislikes';
 const { TextArea } = Input;
 function SingleComment(props) {
     const user = useSelector(state => state.user);
@@ -40,7 +41,8 @@ function SingleComment(props) {
     }
 
     const actions = [
-        <span onClick key="comment-basic-reply-to">Reply to </span>
+        <LikeDislikes comment commentId={props.comment._id} userId={localStorage.getItem('userId')} />,
+        <span onClick={openReply} key="comment-basic-reply-to">Reply to </span>
     ]
 
     return (
